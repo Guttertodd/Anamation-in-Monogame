970cbe8f51b599e536d8a29c2d50624a90c9f74c
339713273 <339713273@2260I-L29-026>
2024-11-11 09:18:51 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Anamation_in_Monogame
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Texture2D greyTribbleTexture;
+        Rectangle greyTribbleRect;
+        Vector2 greyTribbleSpeed;
+
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            greyTribbleRect = new Rectangle(300, 10, 100, 100);
+            greyTribbleSpeed = new Vector2(2, 2);
+            window = new Rectangle(0, 0, 800, 600);
+            _graphics.PreferredBackBufferWidth = window.Width;
+            _graphics.PreferredBackBufferHeight = window.Height;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            greyTribbleTexture = Content.Load<Texture2D>("tribbleGrey");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            greyTribbleRect.X += (int)greyTribbleSpeed.X;
+            if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
+                greyTribbleSpeed.X *= - 1;
+            greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
+            if (greyTribbleRect.Top > window.Height || greyTribbleRect.Bottom < 0)
+                greyTribbleSpeed.Y *= -1;
+           
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(greyTribbleTexture, greyTribbleRect, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Anamation_in_Monogame.Game1();
+game.Run();
