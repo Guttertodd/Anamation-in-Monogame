e0cf1a29daab2130ba8d59c1ed6d07d89d304870
339713273 <339713273@2260I-L29-026>
2024-11-18 09:25:35 -0500

    done

--- Game1.cs
+++ Game1.cs
@@ -11,10 +11,10 @@ namespace Anamation_in_Monogame
 
         Rectangle window;
 
-        Texture2D greyTribbleTexture, brownTribbleTexture;
-        Rectangle greyTribbleRect, brownTribbleRect;
-        Vector2 greyTribbleSpeed, brownTribbleSpeed;
-        Color greyTribble, brownTribble;
+        Texture2D greyTribbleTexture, brownTribbleTexture, creamTribbleTexture, orangeTribbleTexture;
+        Rectangle greyTribbleRect, brownTribbleRect, creamTribbleRect, orangeTribbleRect;
+        Vector2 greyTribbleSpeed, brownTribbleSpeed, creamTribbleSpeed, orangeTribbleSpeed;
+        Color greyTribble, brownTribble, creamTribble, orangeTribble;
 
         Color backgroundColor;
 
@@ -35,6 +35,12 @@ namespace Anamation_in_Monogame
             brownTribbleRect = new Rectangle(100, 10, 100, 100);
             brownTribbleSpeed = new Vector2(2, 2);
 
+            creamTribbleRect = new Rectangle(10,100,100,100);
+            creamTribbleSpeed = new Vector2(2, 2);
+
+            orangeTribbleRect = new Rectangle(300, 100, 100, 100);
+            orangeTribbleSpeed = new Vector2(2, 2);
+
             window = new Rectangle(0, 0, 800, 600);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
@@ -51,6 +57,8 @@ namespace Anamation_in_Monogame
             // TODO: use this.Content to load your game content here
             greyTribbleTexture = Content.Load<Texture2D>("tribbleGrey");
             brownTribbleTexture = Content.Load<Texture2D>("tribbleBrown");
+            creamTribbleTexture = Content.Load<Texture2D>("tribbleCream");
+            orangeTribbleTexture = Content.Load<Texture2D>("tribbleOrange");
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,22 +72,48 @@ namespace Anamation_in_Monogame
             if (greyTribbleRect.Right > window.Width || greyTribbleRect.Left < 0)
             {
                 greyTribbleSpeed.X *= -1;
-                backgroundColor = new Color(136, 252, 3);
+                backgroundColor = new Color(255, 255, 100);
             }
           
             brownTribbleRect.Y += (int)brownTribbleSpeed.Y;
             if (brownTribbleRect.Bottom > window.Height || brownTribbleRect.Top < 0)
             {
                 brownTribbleSpeed.Y *= -1;
-                backgroundColor = new Color(24, 73, 129);
+                backgroundColor = new Color(255, 128, 128);
             }
 
-                //greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
-                //if (greyTribbleRect.Top > window.Height || greyTribbleRect.Bottom < 0)
-                //    greyTribbleSpeed.Y *= -1;
+            creamTribbleRect.Y += (int)creamTribbleSpeed.Y;
+            if (creamTribbleRect.Bottom > window.Height || creamTribbleRect.Top < 0)
+            {
+                creamTribbleSpeed.Y *= -1;
+                backgroundColor = new Color(203, 195, 227);
+            }
+
+            creamTribbleRect.X += (int)creamTribbleSpeed.X;
+            if (creamTribbleRect.Right > window.Width || creamTribbleRect.Left < 0)
+            {
+                creamTribbleSpeed.X *= -1;
+                
+            }
+            orangeTribbleRect.Y += (int)orangeTribbleSpeed.Y;
+            if (orangeTribbleRect.Bottom > window.Height || orangeTribbleRect.Top < 0)
+            {
+                orangeTribbleSpeed.Y *= -1;
+                
+            }
+
+            orangeTribbleRect.X += (int)orangeTribbleSpeed.X;
+            if (orangeTribbleRect.Right > window.Width || orangeTribbleRect.Left < 0)
+            {
+                orangeTribbleSpeed.X *= -1;
+
+            }
+            //greyTribbleRect.Y += (int)greyTribbleSpeed.Y;
+            //if (greyTribbleRect.Top > window.Height || greyTribbleRect.Bottom < 0)
+            //    greyTribbleSpeed.Y *= -1;
 
 
-                base.Update(gameTime);
+            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -91,6 +125,8 @@ namespace Anamation_in_Monogame
 
             _spriteBatch.Draw(greyTribbleTexture, greyTribbleRect, Color.White);
             _spriteBatch.Draw(brownTribbleTexture, brownTribbleRect, Color.White);
+            _spriteBatch.Draw(creamTribbleTexture, creamTribbleRect, Color.White);
+            _spriteBatch.Draw(orangeTribbleTexture, orangeTribbleRect, Color.White);
 
             _spriteBatch.End();
 
